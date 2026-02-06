f6327be866ca7a4850b757c5dad7da5f01f6d53a
shikirato4 <shikirato.ca.z@gmail.com>
2026-02-05 23:19:51 -0600

    Add logger setup function in logger.py
    Implement a logger setup function with file and stream handlers.

---
+++ logger.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import logging
+
+
+def setup_logger(log_file: str) -> logging.Logger:
+    logger = logging.getLogger("jarvis")
+    logger.setLevel(logging.INFO)
+
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
+
+    fh = logging.FileHandler(log_file, encoding="utf-8")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    return logger
